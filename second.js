722bd3fe2f37f840cd87d0c7fe8b3aa31f8f5b5d
Emaan-An <emaansehr7@gmail.com>
2025-07-16 08:29:13 -0700

    Add files via upload

---
+++ second.js
@@ -0,0 +1,16 @@
+import { name } from './first.js'
+console.log(name);
+import { double } from './first.js';
+console.log(double(5))
+// const db = 5*2;
+// console.log(db);
+import {add} from './first.js'
+console.log(add(2,3))
+
+import { username,age } from "./first.js"
+console.log(username)
+console.log(age)
+// const username= document.getElementById("userrname")
+// data.innerHTML=`<h1>Username:${username}</h1>`
+// const age= document.getElementById("age")
+// data.innerHTML=`<h1>username:${age}</h1>`
